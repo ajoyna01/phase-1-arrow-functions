f24a878e220dfdeab0b334889f0c14b4da2763ae
Anna Wilson <ajoyna01@gmail.com>
2021-06-09 19:32:07 -0500

    commit tree dd2c5d5b6e3449daafd0477479726e3861770799

---
+++ index.js
@@ -0,0 +1,14 @@
+// const divide = (num1, num2) => num1 / num2;
+
+//  divide(2000,100);
+// const divide = 2000 / 100; //why doesn't this work?
+
+const divide = function(){
+    return 20;
+}
+
+const square = x => x ** 2;
+    square (2);
+
+const add = (a, b) => (a + b);
+    add(3,4);    
